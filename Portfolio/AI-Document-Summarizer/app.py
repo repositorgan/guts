fa82a08446e1485a93499e275f635ce071c23512
Profit Hero+ <1akieckhefer@gmail.com>
2025-11-20 14:32:31 -0600

    Update app.py
    Comments.

--- Portfolio/AI-Document-Summarizer/app.py
+++ Portfolio/AI-Document-Summarizer/app.py
@@ -1,8 +1,17 @@
+# We want to produce a text file summary. 
+
+# Import functions from module and reading input files.
 from model import summarize_text
 from utils import load_text
 
-
+# Run code when file is executed directly only. Exclude when imported as a script or module.
 if __name__ == "__main__":
+
+# Load raw text file to produce string of text file content.
 text = load_text("sample_input.txt")
+
+# Pass result through function to reduce volume.
 summary = summarize_text(text)
+
+# Deliver consolidation to the console.
 print("SUMMARY:\n", summary)
