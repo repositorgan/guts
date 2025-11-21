b86edfdec512ead091a39023cc4552fe96ed6a88
Profit Hero+ <1akieckhefer@gmail.com>
2025-11-21 11:00:01 -0600

    Update __main__.py

--- Portfolio/AI-Document-Summarizer/__main__.py
+++ Portfolio/AI-Document-Summarizer/__main__.py
@@ -1,4 +1,8 @@
-# Input example.
-# We want to import main() function from: app.y
-# Why? Cross product capacity of project.
-python -m summarizer example.txt
+## Input example.
+## We want to import main() function from: app.y
+## Why? Cross product capacity of project.
+# python -m summarizer example.txt
+from .app import main
+if __name__ == "__main__":
+  ## Call command line interface, (CLI), entry function from: app.py
+  main()
